a24a1ffc30e60987c3e1dc1dc4e29cbb1d841416
Adithya Parthasarathy <adithya91@gmail.com>
2016-04-07 00:32:11 -0400

    N-Gram generation with grammar check

--- CitationGraph/generate_n_grams.py
+++ CitationGraph/generate_n_grams.py
@@ -1,7 +1,36 @@
 import helper
+import time
+
+import nltk
 import nltk.data
 from nltk.util import ngrams
-import time
+from nltk import CFG
+
+import numpy as np
+import os
+
+grammar_string = """
+  S -> TWO | THREE | FOUR
+
+  TWO -> A N | N N | V N | V V | N V | V P
+  THREE -> N N N | A A N | A N N | N A N | N P N | V A N | V N N | A V N | V V N | V P N | A N V | N V V | V D N | V V V | N N V | V V P | V A V | V V N | N C N | V C V | A C A | P A N
+  FOUR -> N C V N | A N N N | N N N N | N P N N | A A N N | A N N N | A N P N | N N P N | N P A N | A C A N | N C N N | N N C N | A N C N | N C A N | P D A N | P N P N | V D N N | V D A N | V V D N
+
+  A -> 'JJ' | 'JJR' | 'JJS'
+  N -> 'NN' | 'NNS' | 'NNP' | 'NNPS' | 'PRP' | 'PRP$'
+  V -> 'VB' | 'VBD' | 'VBG' | 'VBN' | 'VBP' | 'VBZ' | 'RB' | 'RBR' | 'RBS' | 'WRB'
+  P -> 'IN'
+  C -> 'CC' | 'CD'
+  D -> 'DT' | 'PDT' | 'WDT'
+
+  M -> '.' """
+
+grammar = CFG.fromstring(grammar_string)
+
+terminals = {'JJ': None, 'JJR': None, 'JJS': None, 'NN': None, 'NNS': None, 'NNP': None, 'NNPS': None, 'PRP': None,
+             'PRP$': None, 'VB': None, 'VBD': None, 'VBG': None, 'VBN': None, 'VBP': None, 'VBZ': None, 'RB': None,
+             'RBR': None, 'RBS': None, 'WRB': None, 'IN': None, 'CC': None, 'CD': None, 'DT': None, 'PDT': None,
+             'WDT': None}
 
 
 def process_dir(data_dir, MIN_N_GRAM, MAX_N_GRAM):
@@ -10,40 +39,134 @@ def process_dir(data_dir, MIN_N_GRAM, MAX_N_GRAM):
     The n-grams thus generated shall be stored in {data_dir}/n-grams/
     """
 
+    target_dir = os.path.join(data_dir, 'n_grams')
+
+    # Make sure the target directory exists
+    helper.ensure_dir(target_dir)
+
+    # Get the case file list
     case_files = helper.get_files(data_dir)
 
-    case_files = case_files[:10]
+    # case_files = case_files[:5]
 
     for case_file in case_files:
 
+        # Compute the path to save the file
+        target_file_name = os.path.basename(case_file)
+        target_path = os.path.join(target_dir, target_file_name)
+
+        # Read the case data from the string
         case_data = helper.read_file_to_string(case_file)
 
-        # sentences = get_sentences(case_data)
+        valid_n_grams = []
 
-        n_grams = []
-
+        # Go over every sentence in the document
         for sentence in get_sentences(case_data):
-            for n in range(MIN_N_GRAM, MAX_N_GRAM):
 
-                n_grams.extend([str(x) for x in ngrams(sentence.split(), n)])
-                
+            pos_tuples = nltk.pos_tag(nltk.word_tokenize(sentence))
+
+            # Update the grammar if required and get the POS tags
+            pos_tags = get_pos_tags(pos_tuples)
+
+            # Generate N-Grams of tags
+            n_grams = []
+            for n in range(MIN_N_GRAM, MAX_N_GRAM + 1):
+                # n_grams.extend([list(grams) for grams in ngrams(pos_tags, n)])
+                n_grams.extend([list(grams) for grams in ngrams(range(len(pos_tuples)), n)])
+
+            # Get only the n-grams that match the defined grammar
+
+            for i in range(len(n_grams)):
+
+                # Generate n-gram list and check validity
+                if parse([pos_tags[j] for j in n_grams[i]]):
+
+                    # Append words to overall list
+                    valid_n_grams.append([pos_tuples[k][0] for k in n_grams[i]])
+
+        # Save n-grams to file
+        save_list_to_file(target_path, valid_n_grams)
+
+
+def get_pos_tags(pos_tuples):
+    """
+    Returns the POS tags from POS tuples of (word, tag)
+    Updates the grammar for unknown tags
+    """
+
+    global grammar_string
+    global grammar
+    global terminals
+
+    changed_grammar = False
+    pos_tags = []
+
+    for pos_tuple in pos_tuples:
+        tag = pos_tuple[1]
+
+        if tag not in terminals:
+
+            if tag == '\'\'':
+                tag = 'APOS'
+
+            grammar_string += ' | \'' + tag + '\''
+
+            terminals[tag] = None
+            changed_grammar = True
+
+        pos_tags.append(tag)
+
+    if changed_grammar:
+        grammar = CFG.fromstring(grammar_string)
+
+    return pos_tags
+
+
+def save_list_to_file(target_path, list_to_save):
+    """
+    Saves the provided list into the specified path
+    """
+
+    np.savetxt(target_path, list_to_save, fmt='%s')
 
 def get_sentences(case_data):
+    """
+    Returns a list of sentences from a string of possibly many sentences
+    """
 
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-    return tokenizer.tokenize(case_data)
+    return tokenizer.tokenize(case_data.decode('utf8'))
+
+
+def parse(text):
+    """
+    Checks if the provided text has a valid parse
+    """
+
+    parser = nltk.ChartParser(grammar)
+    tree = parser.parse(text)
+
+    valid_parse = False
+
+    for t in tree:
+        valid_parse = True
+        break
+
+    return valid_parse
+
 
 
 def main():
 
     start = time.time()
 
-    MIN_N_GRAM = 3
-    MAX_N_GRAM = 7
+    MIN_N_GRAM = 2
+    MAX_N_GRAM = 4
 
     data_dir = '1881_complete/txt'
     process_dir(data_dir, MIN_N_GRAM, MAX_N_GRAM)
+
 
     print('*** Completed processing in ', time.time() - start, '(s)')
 
